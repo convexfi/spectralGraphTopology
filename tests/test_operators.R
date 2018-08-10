4af53755c74e690825f80e27719f5b61e694933e
mirca <jvmirca@gmail.com>
2018-08-10 14:34:15 -0300

    add more test snippets on the properties of LOp and LStarOp

--- tests/test_operators.R
+++ tests/test_operators.R
@@ -5,11 +5,28 @@ test_that("test_symmetry_of_LOp", {
   # LOp should return a symmetric positive semi-definite matrix
   # with non-positive off diagonal elements and nonnegative
   # diagonal elements
-  W <- LOp(w)
-  expect_that(isSymmetric.matrix(W), is_true())
-  expect_that(all.equal(diag(W) >= 0), is_true())
-  Woff <- W - diag(diag(W))
-  expect_that(all.equal(Woff <= 0), is_true())
-  expect_that(all.equal(colSums(W) == 0), is_true())
-  expect_that(all.equal(rowSums(W) == 0), is_true())
+  Lw <- LOp(w)
+  expect_that(isSymmetric.matrix(Lw), is_true())
+  expect_that(all.equal(diag(Lw) >= 0), is_true())
+  Lw_off <- Lw - diag(diag(Lw))
+  expect_that(all.equal(Lw_off <= 0), is_true())
+  expect_that(all.equal(colSums(Lw) == 0), is_true())
+  expect_that(all.equal(rowSums(Lw) == 0), is_true())
+})
+
+test_that("test_linearity_of_LOp", {
+  # Verify that the implemented L operator is linear
+  a <- runif(1)
+  b <- runif(2)
+  Lw1 <- LOp(w1)
+  Lw2 <- LOp(w2)
+  expect_that(all.equal((a * Lw1 + b * Lw2) == LOp(a * w1 + b * w2)), is_true())
+})
+
+test_that("test_inner_product_relation_between_LOp_and_LStarOp", {
+  # section 1.1 talks about a inner product equality relation
+  # involving LOp and LStarOp, let's verify that
+  Lw <- LOp(w)
+  y <- LStarOp(Y)
+  expect_that(sum(diag(t(Y) %*% Lw)) == w %*% y, is_true())
 })
