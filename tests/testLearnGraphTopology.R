cff71e49326645c4ba9663ba52c251d34d320274
mirca <jvmirca@gmail.com>
2018-08-13 12:16:14 -0300

    add unit test for the diamond graph

--- tests/testLearnGraphTopology.R
+++ tests/testLearnGraphTopology.R
@@ -4,11 +4,18 @@ library(spectralGraphTopology)
 test_that("test_learn_graph_topology", {
   # test the learning of a single-component graph
   n <- 4
-  k <- 100000
+  k <- 10000
   w <- c(1:6)
   K <- 1
-  Lw <- LOp(w, n)
+  Lw <- CppLOp(w, n)
   Y <- t(MASS::mvrnorm(k, as.vector(array(0, n)), MASS::ginv(Lw)))
-  Lw_est <- learnGraphTopology(Y, K, ub=1e4, beta=.1, maxiter=500)
+  Lw_est <- learnGraphTopology(Y, K, ub=100, beta=.1, maxiter=500)
+  expect_that(norm(Lw - Lw_est, type="F") / max(1., norm(Lw, type="F")) < 1e-1, is_true())
+
+  # test the learning of a single-component diamond graph
+  w <- c(1, 1, 0, 1, 1, 1)
+  Lw <- CppLOp(w, n)
+  Y <- t(MASS::mvrnorm(k, as.vector(array(0, n)), MASS::ginv(Lw)))
+  Lw_est <- learnGraphTopology(Y, K, ub=10., beta=1., pho=.5, maxiter=200)
   expect_that(norm(Lw - Lw_est, type="F") / max(1., norm(Lw, type="F")) < 1e-1, is_true())
 })
