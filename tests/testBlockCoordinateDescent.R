5d9715e53368f239f9dbaee0106d175b19358cae
mirca <jvmirca@gmail.com>
2018-08-15 10:56:56 -0300

    add unit test for the case when the upper bound is to small

--- tests/testBlockCoordinateDescent.R
+++ tests/testBlockCoordinateDescent.R
@@ -16,8 +16,8 @@ lambda_update_cvx <- function(lb, ub, beta, U, w, N, K) {
 
 test_that("test_U_update_consistency", {
   # test that U remains orthonormal after being updated
-  w <- runif(6)
-  N <- 4
+  w <- runif(1000)
+  N <- as.integer(.5 * (1 + sqrt(1 + 8 * length(w))))
   K <- 1
   U <- U_update(w, N, K)
   expect_that(all.equal(crossprod(U), diag(array(1., N-K)),
@@ -28,8 +28,8 @@ test_that("test_U_update_consistency", {
 
 test_that("test_lambda_update_consistency", {
   # test that the eigen values meet the criterion after being updated
-  w <- runif(6)
-  N <- 4
+  w <- runif(1000)
+  N <- as.integer(.5 * (1 + sqrt(1 + 8 * length(w))))
   K <- 1
   U <- U_update(w, N, K)
   lb <- 1e-2
@@ -42,10 +42,34 @@ test_that("test_lambda_update_consistency", {
   expect_that(all(lambda[1] >= lb, lambda[q] <= ub,
                   lambda[2:q] >= lambda[1:(q-1)]), is_true())
 
+  # compare against results from CVXR
   lambda_cvx <- lambda_update_cvx(lb, ub, beta, U, w, N, K)
   expect_that(length(lambda_cvx) == q, is_true())
   expect_that(all(lambda_cvx[1] >= lb, lambda_cvx[q] <= ub,
                   lambda_cvx[2:q] >= lambda_cvx[1:(q-1)]), is_true())
 
   expect_that(all(abs(lambda_cvx - lambda) < 1e-3), is_true())
+})
+
+test_that("test_lambda_update_equals_to_ub", {
+  w <- runif(6)
+  N <- as.integer(.5 * (1 + sqrt(1 + 8 * length(w))))
+  K <- 1
+  U <- U_update(w, N, K)
+  lb <- 1e-2
+  ub <- 1.5
+  beta <- .5
+  q <- N - K
+
+  lambda <- lambda_update(lb, ub, beta, U, w, N, K)
+  lambda_cvx <- lambda_update_cvx(lb, ub, beta, U, w, N, K)
+  expect_that(all(abs(lambda_cvx - lambda) < 1e-3), is_true())
+
+  expect_that(length(lambda) == q, is_true())
+  expect_that(all(lambda[1] >= lb, lambda[q] <= ub,
+                  lambda[2:q] >= lambda[1:(q-1)]), is_true())
+
+  expect_that(length(lambda_cvx) == q, is_true())
+  expect_that(all(lambda_cvx[1] >= lb, lambda_cvx[q] <= ub,
+                  lambda_cvx[2:q] >= lambda_cvx[1:(q-1)]), is_true())
 })
