86296f550a82f44e4e73d9fe0235311090471f3a
mirca <jvmirca@gmail.com>
2018-08-17 11:05:16 -0300

    move objective function to its own file

---
+++ R/objectiveFunction.R
@@ -0,0 +1,4 @@
+objFunction <- function(Theta, U, lambda, Km, beta, N, K) {
+  return(sum(-log(lambda)) + sum(diag(Km %*% Theta)) +
+         .5 * beta * norm(Theta - crossprod(sqrt(lambda) * t(U)), type="F")^2)
+}
