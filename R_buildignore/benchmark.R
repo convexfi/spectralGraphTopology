fb6703f557a9350c49ba5e7416da4ff03b217f70
mirca <jvmirca@gmail.com>
2018-08-22 07:17:49 -0300

    naive benchmark updating beta

--- R_buildignore/benchmark.R
+++ R_buildignore/benchmark.R
@@ -11,9 +11,9 @@ library(ggplot2)
 # algorithms.
 warmup_benchmark <- function(N_realizations, N_nodes_max = 10) {
   # fix the number of observations
-  T <- 50
+  T <- 200
   pb <- txtProgressBar(min = 3, max = N_nodes_max, style = 3)
-  N_nodes_set <- seq(3, N_nodes_max, by = 1)
+  N_nodes_set <- seq(3, N_nodes_max, by = 3)
   rel_err_spec <- array(0, length(N_nodes_set))
   rel_err_naive <- array(0, length(N_nodes_set))
   rel_err_qp <- array(0, length(N_nodes_set))
@@ -28,7 +28,7 @@ warmup_benchmark <- function(N_realizations, N_nodes_max = 10) {
       Lw <- L(w)
       Y <- MASS::mvrnorm(T, rep(0, N), MASS::ginv(Lw))
       covY <- cov(Y)
-      res <- learnGraphTopology(Y, K = 1, beta = 50)
+      res <- learnGraphTopology(Y, K = 1, beta = 1, maxiter_beta = 50)
       Lw_est <- res$Lw
       rel_err["spectral"] <- rel_err["spectral"] + relativeError(Lw, Lw_est)
       rel_err["naive"] <- rel_err["naive"] + relativeError(Lw, naive(covY))
@@ -43,9 +43,10 @@ warmup_benchmark <- function(N_realizations, N_nodes_max = 10) {
   x <- T / N_nodes_set
   df <- data.frame(x, rel_err_spec, rel_err_naive, rel_err_qp)
   ggplot(df, aes(x)) +
-    geom_line(aes(y=rel_err_spec), colour="red") +
-    geom_line(aes(y=rel_err_naive), colour="green") +
-    geom_line(aes(y=rel_err_qp), colour="blue")
+    geom_line(aes(y=rel_err_spec), colour="red") + geom_point(aes(y=rel_err_spec), colour="red") +
+    geom_line(aes(y=rel_err_naive), colour="green") + geom_point(aes(y=rel_err_naive), colour="green") +
+    geom_line(aes(y=rel_err_qp), colour="blue") + geom_point(aes(y=rel_err_qp), colour="blue") +
+    labs(y="Relative Error (%)", x="T/N")
 }
 
 # naive estimator
@@ -67,5 +68,5 @@ relativeError <- function(Xtrue, Xest) {
 }
 
 # usage
-warmup_benchmark(N_realizations = 100, N_nodes_max = 10)
+warmup_benchmark(N_realizations = 100, N_nodes_max = 30)
 warnings()
