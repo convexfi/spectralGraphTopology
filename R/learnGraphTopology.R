8ef003a3d7114aae307a9cd3e25cca52c7ebd49e
mirca <jvmirca@gmail.com>
2018-09-07 16:11:28 -0300

    polish the code and return the sequence of estimates of w

--- R/learnGraphTopology.R
+++ R/learnGraphTopology.R
@@ -28,6 +28,7 @@
 #' @return w the optimal value of the weight vector
 #' @return lambda the optimal value of the eigenvalues
 #' @return U the optimal value of the eigenvectors
+#'
 #' @author Convex group - HKUST
 #' @references our paper soon to be submitted
 #' @examples
@@ -49,7 +50,7 @@
 #' norm(Lw - res$Lw, type="F") / norm(Lw, type="F")
 #' @export
 learnGraphTopology <- function (Y, K, w0 = "qp", lb = 1e-4, ub = 1e4, alpha = 0.,
-                                beta = .5, rho = .1, maxiter = 5000, maxiter_beta = 1,
+                                beta = 10., rho = .1, maxiter = 5000, maxiter_beta = 1,
                                 w_tol = 1e-6, lambda_tol = 1e-6, U_tol = 1e-6,
                                 ftol = 1e-6) {
   # number of samples per node
@@ -61,7 +62,6 @@ learnGraphTopology <- function (Y, K, w0 = "qp", lb = 1e-4, ub = 1e4, alpha = 0.
   # l1-norm penalty factor
   H <- alpha * (2 * diag(N) - matrix(1, N, N))
   Kmat <- S + H
-
   # find an appropriate inital guess
   Sinv <- MASS::ginv(S)
   if (w0 == "qp") {
@@ -74,37 +74,36 @@ learnGraphTopology <- function (Y, K, w0 = "qp", lb = 1e-4, ub = 1e4, alpha = 0.
   evd <- eigen(L(w0))
   lambda0 <- pmax(0, evd$values[(N-K):1])
   U0 <- evd$vectors[, (N-K):1]
-
+  # save objective function value at initial guess
   ll0 <- logLikelihood(L(w0), lambda0, Kmat)
   fun0 <- ll0 + logPrior(beta, L(w0), lambda0, U0)
   fun_seq <- c(fun0)
   ll_seq <- c(ll0)
+  w_seq <- list(w0)
 
   for (i in 1:maxiter_beta) {
     for (k in 1:maxiter) {
       w <- w_update(w0, U0, beta, lambda0, N, Kmat)
       U <- U_update(w, N, K)
       lambda <- lambda_update(lb, ub, beta, U, w, N, K)
-
-      # check tolerance on parameters
+      # compute relative error on the parameters
       w_err <- norm(w - w0, type="2") / max(1, norm(w, type="2"))
       lambda_err <- norm(lambda - lambda0, type="2") / max(1, norm(lambda, type="2"))
       U_err <- norm(U - U0, type="F") / max(1, norm(U, type="2"))
-
+      # check tolerance on the parameters
       if ((w_err < w_tol) & (lambda_err < lambda_tol) & (U_err < U_tol))
         break
-
-      # check tolerance on objective function
+      # compute relative error on the objective function
       ll <- logLikelihood(L(w), lambda, Kmat)
       fun <- ll + logPrior(beta, L(w), lambda, U)
       ferr <- abs(fun - fun0) / max(1, abs(fun))
-      ll_seq <- c(ll_seq, ll)
-      fun_seq <- c(fun_seq, fun)
-
-      # check tolerance on objective function
+      # check tolerance on the objective function
       if (ferr < ftol)
         break
-
+      # save and update estimates
+      ll_seq <- c(ll_seq, ll)
+      fun_seq <- c(fun_seq, fun)
+      w_seq <- rlist::list.append(w_seq, w)
       fun0 <- fun
       w0 <- w
       U0 <- U
@@ -112,8 +111,10 @@ learnGraphTopology <- function (Y, K, w0 = "qp", lb = 1e-4, ub = 1e4, alpha = 0.
     }
     beta <- beta * (1 + rho)
   }
+  # compute the Laplacian matrix of the best w
   Lw <- L(w)
+  # compute the adjancency matrix
   W <- diag(diag(Lw)) - Lw
   return(list(Lw = Lw, W = W, obj_fun = fun_seq, loglike = ll_seq,
-              w = w, lambda = lambda, U = U))
+              w_seq = w_seq, w = w, lambda = lambda, U = U))
 }
