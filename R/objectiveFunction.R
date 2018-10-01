7fce0a52c7c1b2ab3841f62e1c4878989eb63d2d
mirca <jvmirca@gmail.com>
2018-10-01 19:42:45 -0300

    be consistent on nomenclature

--- R/objectiveFunction.R
+++ R/objectiveFunction.R
@@ -1,14 +1,14 @@
 # Negative log of the posterior density function
-objectiveFunction <- function(Theta, U, lambda, Kmat, beta) {
-  return(logLikelihood(Theta, lambda, Kmat) + logPrior(beta, Theta, lambda, U))
+objectiveFunction <- function(Lw, U, lambda, Kmat, beta) {
+  return(logLikelihood(Lw, lambda, Kmat) + logPrior(beta, Lw, lambda, U))
 }
 
 # Negative logarithm of the likelihood function
-logLikelihood <- function(Theta, lambda, Kmat) {
-  return(sum(-log(lambda)) + sum(diag(Kmat %*% Theta)))
+logLikelihood <- function(Lw, lambda, Kmat) {
+  return(sum(-log(lambda)) + sum(diag(Kmat %*% Lw)))
 }
 
 # Negative logarithm of the prior probability distribution
-logPrior <- function(beta, Theta, lambda, U) {
-  return(.5 * beta * norm(Theta - crossprod(sqrt(lambda) * t(U)), type="F")^2)
+logPrior <- function(beta, Lw, lambda, U) {
+  return(.5 * beta * norm(Lw - crossprod(sqrt(lambda) * t(U)), type="F")^2)
 }
