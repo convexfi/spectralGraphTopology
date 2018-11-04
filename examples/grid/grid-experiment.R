d8ba28063973cf6cc37cd1ab284c16f438815675
mirca <jvmirca@gmail.com>
2018-11-03 22:07:37 -0300

    update grid plots

--- examples/grid/grid-experiment.R
+++ examples/grid/grid-experiment.R
@@ -1,17 +1,28 @@
 library(igraph)
-library(viridis)
+library(R.matlab)
 library(spectralGraphTopology)
+library(latex2exp)
+library(colorspace)
 
-N_realizations <- 100
+N_realizations <- 10
 ratios <- c(.5, .75, 1, 5, 10, 30, 100, 250, 500, 1000)
-#ratios <- c(100, 250, 500, 1000)
+n_ratios <- c(1:length(ratios))
 # design synthetic Laplacian of a grid graph
 N <- 64
 grid <- make_lattice(length = sqrt(N), dim = 2)
-# sample edges weights from U(.1, 3)
-rel_err <- array(0, length(ratios))
-fscore <- array(0, length(ratios))
-for (j in c(1:length(ratios))) {
+rel_err_spec <- array(0, length(ratios))
+rel_err_ggl <- array(0, length(ratios))
+rel_err_naive <- array(0, length(ratios))
+fscore_spec <- array(0, length(ratios))
+fscore_ggl <- array(0, length(ratios))
+fscore_naive <- array(0, length(ratios))
+
+matlab <- Matlab()
+open(matlab)
+A_mask <- matrix(1, 64, 64) - diag(64)
+setVariable(matlab, A_mask = A_mask)
+
+for (j in n_ratios) {
   T <- as.integer(ratios[j] * N)
   cat("\nRunning simulation for", T, "samples per node, T/N = ", ratios[j], "\n")
   for (n in 1:N_realizations) {
@@ -20,15 +31,20 @@ for (j in c(1:length(ratios))) {
     # sample data from GP with covariance matrix set as
     # the pseudo inverse of the true Laplacian
     Y <- MASS::mvrnorm(T, mu = rep(0, N), Sigma = MASS::ginv(Ltrue))
+    S <- crossprod(Y) / T
     # run spectralGraphTopology
     if (ratios[j] <= 1)
-      graph <- learnGraphTopology(crossprod(Y) / T, w0 = "naive",
-                                  beta = 1e-2, beta_max = 4, npoints = 5)
+      graph <- learnGraphTopology(S, w0 = "naive", beta = 1e-2, beta_max = 4, nbeta = 10)
     else
-      graph <- learnGraphTopology(crossprod(Y) / T, w0 = "naive", beta = 4)
+      graph <- learnGraphTopology(S, w0 = "naive", beta = 4)
     print(graph$convergence)
-    # run GLasso
-    # graph_lasso <- glasso(crossprod(Y) / T, rho = 5e-3)
+
+    # compute naive
+    Lnaive <- MASS::ginv(S)
+    # set data variable to MATLAB
+    setVariable(matlab, S = S)
+    evaluate(matlab, "[Lggl,~,~] = estimate_ggl(S, A_mask, 1e-3, 1e-4 , 1e-6, 40, 2)")
+    Lggl <- getVariable(matlab, "Lggl")
     # create an igraph object from the estimated and true adjacency matrices
     #est_net <- graph_from_adjacency_matrix(graph$W, mode = "undirected", weighted = TRUE)
     ##lasso_net <- graph_from_adjacency_matrix(diag(diag(graph_lasso$wi)) - graph_lasso$wi, mode = "undirected", weighted = TRUE)
@@ -44,48 +60,79 @@ for (j in c(1:length(ratios))) {
     ##lasso_est <- layout_on_grid(lasso_net)
     #la_true <- layout_on_grid(true_net)
     # print numerical results
-    rel = relativeError(Ltrue, graph$Lw)
-    fs = Fscore(Ltrue, graph$Lw, 1e-3)
-    print(rel)
-    print(fs)
-    #print(relativeError(Ltrue, graph_lasso$wi))
-    rel_err[j] <- rel_err[j] + rel
-    fscore[j] <- fscore[j] + fs
+    rel_spec = relativeError(Ltrue, graph$Lw)
+    fs_spec = Fscore(Ltrue, graph$Lw, 1e-3)
+    rel_ggl = relativeError(Ltrue, Lggl$Lggl)
+    fs_ggl = Fscore(Ltrue, Lggl$Lggl, 1e-3)
+    rel_naive = relativeError(Ltrue, Lnaive)
+    fs_naive = Fscore(Ltrue, Lnaive, 1e-3)
+    print(rel_spec)
+    print(fs_spec)
+    print(rel_ggl)
+    print(fs_ggl)
+    print(rel_naive)
+    print(fs_naive)
+    rel_err_spec[j] <- rel_err_spec[j] + rel_spec
+    fscore_spec[j] <- fscore_spec[j] + fs_spec
+    rel_err_ggl[j] <- rel_err_ggl[j] + rel_ggl
+    fscore_ggl[j] <- fscore_ggl[j] + fs_ggl
+    rel_err_naive[j] <- rel_err_naive[j] + rel_naive
+    fscore_naive[j] <- fscore_naive[j] + fs_naive
   }
-  rel_err[j] <- rel_err[j] / N_realizations
-  fscore[j] <- fscore[j] / N_realizations
+  rel_err_spec[j] <- rel_err_spec[j] / N_realizations
+  fscore_spec[j] <- fscore_spec[j] / N_realizations
+  rel_err_ggl[j] <- rel_err_ggl[j] / N_realizations
+  fscore_ggl[j] <- fscore_ggl[j] / N_realizations
+  rel_err_naive[j] <- rel_err_naive[j] / N_realizations
+  fscore_naive[j] <- fscore_naive[j] / N_realizations
   cat("\n** spectralGraphTopology results **\n")
   cat("Avg Relative error: ")
-  cat(rel_err[j], "\n")
+  cat(rel_err_spec[j], "\n")
   cat("Avg Fscore: ")
-  cat(fscore[j], "\n")
+  cat(fscore_spec[j], "\n")
+  cat("\n** GGL results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_ggl[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_ggl[j], "\n")
+  cat("\n** Naive results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_naive[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_naive[j], "\n")
 }
 
+colors <- rainbow_hcl(3)
 gr = .5 * (1 + sqrt(5))
 setEPS()
 postscript("relative_error_grid.ps", family = "Times", height = 5, width = gr * 3.5)
-plot(c(1:length(ratios)), rel_err, type = "b", pch=19, cex=.6,
-     ylim=c(min(rel_err) - 1e-2, max(rel_err) + .1e-2),
-     xlab = "T / N", ylab = "Average Relative Error", col = "black", xaxt = "n")
-axis(side = 1, at = c(1:length(ratios)), labels = ratios)
+plot(n_ratios, rel_err_naive, type = "b", pch=19, cex=.6,
+     ylim=c(0, .6),
+     xlab = TeX("$T / N$"), ylab = "Average Relative Error", col = colors[1], xaxt = "n")
+lines(n_ratios, rel_err_ggl, type = "b", pch=19, cex=.6, col = colors[2], xaxt = "n")
+lines(n_ratios, rel_err_spec, type = "b", pch=19, cex=.6, col = colors[3], xaxt = "n")
+axis(side = 1, at = n_ratios, labels = ratios)
+legend("topright", legend=c("ISCM", "GGL", "SGT"), col=colors, lty=c(1, 1, 1), cex=0.8)
 dev.off()
 setEPS()
 postscript("fscore_grid.ps", family = "Times", height = 5, width = gr * 3.5)
-plot(c(1:length(ratios)), fscore, type = "b", pch=19, cex=.6,
-     ylim=c(min(fscore) - 1e-2, 1.),
-     xlab = "T / N", ylab = "Average F-score", col = "black", xaxt = "n")
-axis(side = 1, at = c(1:length(ratios)), labels = ratios)
+plot(n_ratios, fscore_naive, type = "b", pch=19, cex=.6,
+     ylim=c(0, 1.), xlab = TeX("$T / N$"), ylab = "Average F-score", col = colors[1], xaxt = "n")
+lines(n_ratios, fscore_ggl, type = "b", pch=19, cex=.6, col = colors[2], xaxt = "n")
+lines(n_ratios, fscore_spec, type = "b", pch=19, cex=.6, col = colors[3], xaxt = "n")
+axis(side = 1, at = n_ratios, labels = ratios)
+legend("topleft", legend=c("ISCM", "GGL", "SGT"), col=colors, lty=c(1, 1, 1), cex=0.8)
 dev.off()
 
-#cat("** glasso results **\n")
+#cat("** gggl results **\n")
 #cat("Relative error: ")
-#cat(relativeError(Ltrue, graph_lasso$wi), "\n")
+#cat(relativeError(Ltrue, graph_ggl$wi), "\n")
 #cat("Fscore: ")
-#cat(Fscore(Ltrue, graph_lasso$wi, 1e-3))
+#cat(Fscore(Ltrue, graph_ggl$wi, 1e-3))
 # plot graphs
 #plot(true_net, layout = la_true, vertex.label = NA, vertex.size = 3, main = "True Graph")
 #plot(est_net, layout = la_est, vertex.label = NA, vertex.size = 3, main = "spectralGraphTopology Graph")
-##plot(lasso_net, layout = lasso_est, vertex.label = NA, vertex.size = 3, main = "GLasso Graph")
+##plot(ggl_net, layout = ggl_est, vertex.label = NA, vertex.size = 3, main = "GLasso Graph")
 #plot(graph$elapsed_time, graph$obj_fun, type = "b", pch=19, cex=.6, col = scales::alpha("black", .5),
 #     xlab = "CPU time [seconds]", ylab = "Objective function")
 
