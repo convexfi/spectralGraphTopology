ab3fa78e8c6b34f82bcd9e1eeb5fda2f1d5b2606
mirca <jvmirca@gmail.com>
2018-12-03 21:50:05 -0300

    working on modular graph benchmark

--- examples/block-diagonal/block-diag-performance-T-over-N.R
+++ examples/block-diagonal/block-diag-performance-T-over-N.R
@@ -4,54 +4,66 @@ library(spectralGraphTopology)
 library(extrafont)
 library(latex2exp)
 
-N_realizations <- 1
-ratios <- c(500, 1000)
+set.seed(12)
+N_realizations <- 20
+ratios <- c(.5, .75, 1., 5., 10, 30, 100, 250, 500, 1000)
 n_ratios <- c(1:length(ratios))
 rel_err_spec <- array(0, length(ratios))
+rel_err_cgl <- array(0, length(ratios))
 rel_err_cglA <- array(0, length(ratios))
 rel_err_naive <- array(0, length(ratios))
 fscore_spec <- array(0, length(ratios))
+fscore_cgl <- array(0, length(ratios))
 fscore_cglA <- array(0, length(ratios))
 fscore_naive <- array(0, length(ratios))
 
 print("Connecting to MATLAB...")
-matlab <- Matlab(port=9997)
+matlab <- Matlab(port=9999)
 is_matlab_open <- open(matlab)
 print(is_matlab_open)
 
 N <- 64
 K <- 4
-P <- diag(1, K)
+P <- diag(.49, K) + 0.01
 # K-component graph
-mgraph <- sample_sbm(N, pref.matrix = P, block.sizes = c(rep(N / K, K)))
-A_mask <- as.matrix(laplacian_matrix(mgraph)) != 0
+A_mask <- matrix(1, N, N) - diag(N)
 setVariable(matlab, A_mask = A_mask)
 
 for (j in n_ratios) {
-  fails <- 0
   T <- as.integer(ratios[j] * N)
   cat("\nRunning simulation for", T, "samples per node, T/N = ", ratios[j], "\n")
   for (n in 1:N_realizations) {
+    mgraph <- sample_sbm(N, pref.matrix = P, block.sizes = c(rep(N / K, K)))
+    A_modular_mask <- as.matrix(laplacian_matrix(mgraph)) != 0
+    setVariable(matlab, A_modular_mask = A_modular_mask)
     E(mgraph)$weight <- runif(gsize(mgraph), min = 1e-1, max = 3)
     Ltrue <- as.matrix(laplacian_matrix(mgraph))
     Y <- MASS::mvrnorm(T, mu = rep(0, N), Sigma = MASS::ginv(Ltrue))
-    S <- cov(Y) + (1/3) * diag(N)
+    S <- cov(Y)
     s_max <- max(abs(S - diag(diag(S))))
     alphas <- c(.75 ^ (c(1:14)) * s_max * sqrt(log(N)/ T), 0)
     setVariable(matlab, S = S)
-    if (ratios[j] <= 5) {
-      graph <- learnGraphTopology(S, w0 = "naive", K = K, beta = 1e-3, beta_max = 2, nbeta = 20)
-    } else {
-      graph <- learnGraphTopology(S, w0 = "naive", K = K, beta = 2)
-    }
+    graph <- learnGraphTopology(S, w0 = "naive", K = 1, ub = 32, beta = 4, maxiter = 100000)
+    print(graph$convergence)
+    print(graph$lambda)
+    print(relativeError(Ltrue, graph$Lw))
+    print(Fscore(Ltrue, graph$Lw, 1e-1))
+
     rel_cgl <- 9999999999
     rel_cglA <- 9999999999
     for (alpha in alphas) {
       setVariable(matlab, alpha = alpha)
-      evaluate(matlab, "[LcglA,~,~] = estimate_cgl(S, A_mask, alpha, 1e-4, 1e-6, 40, 1)")
+      evaluate(matlab, "[Lcgl,~,~] = estimate_cgl(S, A_mask, alpha, 1e-6, 1e-6, 40, 1)")
+      evaluate(matlab, "[LcglA,~,~] = estimate_cgl(S, A_modular_mask, alpha, 1e-6, 1e-6, 40, 1)")
+      Lcgl <- getVariable(matlab, "Lcgl")
       LcglA <- getVariable(matlab, "LcglA")
-      if (anyNA(LcglA$LcglA)) {
+      if (anyNA(Lcgl$Lcgl) || anyNA(LcglA$LcglA)) {
         next
+      }
+      tmp_rel_cgl <- relativeError(Ltrue, Lcgl$Lcgl)
+      if (tmp_rel_cgl < rel_cgl) {
+        rel_cgl <- tmp_rel_cgl
+        fs_cgl <- Fscore(Ltrue, Lcgl$Lcgl, 1e-1)
       }
       tmp_rel_cglA <- relativeError(Ltrue, LcglA$LcglA)
       if (tmp_rel_cglA < rel_cglA) {
@@ -60,28 +72,25 @@ for (j in n_ratios) {
       }
     }
     Lnaive <- MASS::ginv(S)
-    # set data variable to MATLAB
     rel_spec = relativeError(Ltrue, graph$Lw)
     fs_spec = Fscore(Ltrue, graph$Lw, 1e-1)
     rel_naive = relativeError(Ltrue, Lnaive)
     fs_naive = Fscore(Ltrue, Lnaive, 1e-1)
-    print(rel_spec)
-    print(fs_spec)
-    print(rel_cglA)
-    print(fs_cglA)
-    print(rel_naive)
-    print(fs_naive)
     rel_err_spec[j] <- rel_err_spec[j] + rel_spec
     fscore_spec[j] <- fscore_spec[j] + fs_spec
     rel_err_cglA[j] <- rel_err_cglA[j] + rel_cglA
     fscore_cglA[j] <- fscore_cglA[j] + fs_cglA
+    rel_err_cgl[j] <- rel_err_cgl[j] + rel_cgl
+    fscore_cgl[j] <- fscore_cgl[j] + fs_cgl
     rel_err_naive[j] <- rel_err_naive[j] + rel_naive
     fscore_naive[j] <- fscore_naive[j] + fs_naive
   }
   rel_err_spec[j] <- rel_err_spec[j] / N_realizations
   fscore_spec[j] <- fscore_spec[j] / N_realizations
-  rel_err_cglA[j] <- rel_err_cglA[j] / (N_realizations - fails)
-  fscore_cglA[j] <- fscore_cglA[j] / (N_realizations - fails)
+  rel_err_cglA[j] <- rel_err_cglA[j] / N_realizations
+  fscore_cglA[j] <- fscore_cglA[j] / N_realizations
+  rel_err_cgl[j] <- rel_err_cgl[j] / N_realizations
+  fscore_cgl[j] <- fscore_cgl[j] / N_realizations
   rel_err_naive[j] <- rel_err_naive[j] / N_realizations
   fscore_naive[j] <- fscore_naive[j] / N_realizations
   cat("\n** spectralGraphTopology results **\n")
@@ -89,6 +98,11 @@ for (j in n_ratios) {
   cat(rel_err_spec[j], "\n")
   cat("Avg Fscore: ")
   cat(fscore_spec[j], "\n")
+  cat("\n** CGL results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_cgl[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_cgl[j], "\n")
   cat("\n** CGL with A results **\n")
   cat("Avg Relative error: ")
   cat(rel_err_cglA[j], "\n")
@@ -101,29 +115,31 @@ for (j in n_ratios) {
   cat(fscore_naive[j], "\n")
 }
 
-colors <- c("#0B032D", "#F67E7D", "#FFB997")
+colors <- c("#0B032D", "#843B62", "#F67E7D", "#FFB997")
 gr = .5 * (1 + sqrt(5))
 setEPS()
-postscript("relative_error_block_diagonal.ps", family = "ComputerModern", height = 5, width = gr * 3.5)
+postscript("relative_error_modular.ps", family = "ComputerModern", height = 5, width = gr * 3.5)
 plot(n_ratios, rel_err_naive, type = "b", pch=15, cex=.75, ylim=c(0, .5),
      xlab = TeX("$\\mathit{T} / \\mathit{N}$"), ylab = "Average Relative Error", col = colors[1], xaxt = "n")
 grid()
-lines(n_ratios, rel_err_cglA, type = "b", pch=17, cex=.75, col = colors[2], xaxt = "n")
-lines(n_ratios, rel_err_spec, type = "b", pch=18, cex=.85, col = colors[3], xaxt = "n")
+lines(n_ratios, rel_err_cgl, type = "b", pch=16, cex=.75, col = colors[2], xaxt = "n")
+lines(n_ratios, rel_err_cglA, type = "b", pch=17, cex=.75, col = colors[3], xaxt = "n")
+lines(n_ratios, rel_err_spec, type = "b", pch=18, cex=.85, col = colors[4], xaxt = "n")
 axis(side = 1, at = n_ratios, labels = ratios)
-legend("topright", legend = c("ISCM", TeX("CGL($\\mathbf{A}$)"), "SGL"),
-       col=colors, pch=c(15, 17, 18), lty=c(1, 1, 1), bty="n")
+legend("topright", legend = c("ISCM", "CGL", TeX("CGL($\\mathbf{A}$)"), "SGL"),
+       col=colors, pch=c(15, 16, 17, 18), lty=c(1, 1, 1, 1), bty="n")
 dev.off()
-embed_fonts("relative_error_block_diagonal.ps", outfile="relative_error_block_diagonal.ps")
+embed_fonts("relative_error_modular.ps", outfile="relative_error_modular.ps")
 setEPS()
-postscript("fscore_block_diagonal.ps", family = "ComputerModern", height = 5, width = gr * 3.5)
-plot(n_ratios, fscore_naive, ylim=c(.3, 1.), xlab = TeX("$\\mathit{T} / \\mathit{N}$"),
+postscript("fscore_grid.ps", family = "ComputerModern", height = 5, width = gr * 3.5)
+plot(n_ratios, fscore_naive, ylim=c(.4, 1.), xlab = TeX("$\\mathit{T} / \\mathit{N}$"),
      ylab = "Average F-score", type = "b", pch=15, cex=.75, col = colors[1], xaxt = "n")
 grid()
-lines(n_ratios, fscore_cglA, type = "b", pch=17, cex=.75, col = colors[2], xaxt = "n")
-lines(n_ratios, fscore_spec, type = "b", pch=18, cex=.85, col = colors[3], xaxt = "n")
+lines(n_ratios, fscore_cgl, type = "b", pch=16, cex=.75, col = colors[2], xaxt = "n")
+lines(n_ratios, fscore_cglA, type = "b", pch=17, cex=.75, col = colors[3], xaxt = "n")
+lines(n_ratios, fscore_spec, type = "b", pch=18, cex=.85, col = colors[4], xaxt = "n")
 axis(side = 1, at = n_ratios, labels = ratios)
-legend("bottomright", legend = c("ISCM", TeX("CGL($\\mathbf{A}$)"), "SGL"),
-       col=colors, pch=c(15, 17, 18), lty=c(1, 1, 1), bty="n")
+legend("bottomright", legend = c("ISCM", "CGL", TeX("CGL($\\mathbf{A}$)"), "SGL"),
+       col=colors, pch=c(15, 16, 17, 18), lty=c(1, 1, 1, 1), bty="n")
 dev.off()
-embed_fonts("fscore_block_diagonal.ps", outfile="fscore_block_diagonal.ps")
+embed_fonts("fscore_modular.ps", outfile="fscore_modular.ps")
