3324c5306211d1b827a23eca620815a6405b2a19
mirca <jvmirca@gmail.com>
2018-12-03 22:05:26 -0300

    minor updates

--- examples/modular/modular-experiment.R
+++ examples/modular/modular-experiment.R
@@ -2,16 +2,19 @@ library(igraph)
 library(viridis)
 library(spectralGraphTopology)
 
+set.seed(0)
 N <- 64
-T <- 30 * N
-P <- diag(.5, 4)
+#T <- 30 * N
+T <- 100 * N
+P <- diag(.49, 4) + 0.01
 mgraph <- sample_sbm(N, pref.matrix = P, block.sizes = c(rep(16, 4)))
-E(mgraph)$weight <- runif(gsize(mgraph), min = 1e-1, max = 3)
+E(mgraph)$weight <- runif(gsize(mgraph), min = 1e-1, max = 1)
 Ltrue <- as.matrix(laplacian_matrix(mgraph))
 Y <- MASS::mvrnorm(T, mu = rep(0, N), Sigma = MASS::ginv(Ltrue))
-w0 <- solve(Mmat(N * (N - 1) / 2), Lstar(MASS::ginv(cov(Y))))
-graph <- learnGraphTopology(cov(Y), K = 4, w0 = "qp", beta = 1)
-est_net <- graph_from_adjacency_matrix(graph$W, mode = "undirected", weighted = TRUE)
+graph <- learnGraphTopology(cov(Y), K = 1, w0 = "qp", beta = 5)
+W <- graph$W
+W[W < 1e-1] = 0
+est_net <- graph_from_adjacency_matrix(W, mode = "undirected", weighted = TRUE)
 true_net <- graph_from_adjacency_matrix(diag(diag(Ltrue)) - Ltrue, mode = "undirected", weighted = TRUE)
 colors <- inferno(5, begin = 0, end = 1, direction = -1)
 c_scale <- colorRamp(colors)
