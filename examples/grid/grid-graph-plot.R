6d20377e1f5928f68b7eb10eee8c6db33304f4ee
mirca <jvmirca@gmail.com>
2018-12-08 08:42:37 -0300

    experiments updates

--- examples/grid/grid-graph-plot.R
+++ examples/grid/grid-graph-plot.R
@@ -2,6 +2,8 @@ library(igraph)
 library(spectralGraphTopology)
 library(viridis)
 library(corrplot)
+library(scales)
+library(R.matlab)
 
 set.seed(0)
 
@@ -14,39 +16,57 @@ Wtrue <- diag(diag(Ltrue)) - Ltrue
 Y <- MASS::mvrnorm(T, mu = rep(0, N), Sigma = MASS::ginv(Ltrue))
 S <- cov(Y)
 
-graph <- learnGraphTopology(S, w0 = "naive", beta = 4)
-W <- diag(diag(graph$Lw)) - graph$Lw
-Lnaive <- MASS::ginv(S)
-Wnaive <- diag(diag(Lnaive)) - Lnaive
-Wnaive[abs(Wnaive) < 1e-2] <- 0
-grid_est <- graph_from_adjacency_matrix(W, mode = "undirected", weighted = TRUE)
-grid_naive <- graph_from_adjacency_matrix(Wnaive, mode = "undirected", weighted = TRUE)
+print("Connecting to MATLAB...")
+matlab <- Matlab()
+open(matlab)
+print("success!")
+A_mask <- matrix(1, 64, 64) - diag(64)
+setVariable(matlab, A_mask = A_mask)
+setVariable(matlab, S = S)
+alpha = 0.003584478
+setVariable(matlab, alpha = alpha)
+evaluate(matlab, "[Lcgl,~,~] = estimate_cgl(S, A_mask, alpha, 1e-6, 1e-6, 40, 1)")
+Lcgl <- getVariable(matlab, "Lcgl")
+graph <- learnGraphTopology(S, w0 = "naive", beta = 10)
 
-colors <- inferno(5, begin = 0, end = 1, direction = -1)
+# compute adjacency matrices
+W <- diag(diag(graph$Lw)) - graph$Lw
+W[W < 5e-2] <- 0
+W_cgl <- diag(diag(Lcgl$Lcgl)) - Lcgl$Lcgl
+W_cgl[W_cgl < 5e-2] <- 0
+# compute grids
+grid_spec <- graph_from_adjacency_matrix(W, mode = "undirected", weighted = TRUE)
+grid_cgl <- graph_from_adjacency_matrix(W_cgl, mode = "undirected", weighted = TRUE)
+
+colors <- viridis(5, begin = 0, end = 1, direction = -1)
 c_scale <- colorRamp(colors)
-E(grid_est)$color = apply(c_scale(E(grid_est)$weight / max(E(grid_est)$weight)), 1,
+E(grid_spec)$color = apply(c_scale(E(grid_spec)$weight / max(E(grid_spec)$weight)), 1,
+                          function(x) rgb(x[1]/255, x[2]/255, x[3]/255))
+E(grid_cgl)$color = apply(c_scale(E(grid_cgl)$weight / max(E(grid_cgl)$weight)), 1,
                           function(x) rgb(x[1]/255, x[2]/255, x[3]/255))
 E(grid)$color = apply(c_scale(E(grid)$weight / max(E(grid)$weight)), 1,
                       function(x) rgb(x[1]/255, x[2]/255, x[3]/255))
-E(grid_naive)$color = apply(c_scale(abs(E(grid_naive)$weight) / max(E(grid_naive)$weight)), 1,
-                            function(x) rgb(x[1]/255, x[2]/255, x[3]/255))
 
-la_est <- layout_on_grid(grid_est)
+la_spec <- layout_on_grid(grid_spec)
+la_cgl <- layout_on_grid(grid_cgl)
 la_true <- layout_on_grid(grid)
-la_naive <- layout_on_grid(grid_naive)
 
-rel_spec <- relativeError(Ltrue, graph$Lw)
-fs_spec <- Fscore(Ltrue, graph$Lw, 1e-2)
-rel_naive <- relativeError(Ltrue, Lnaive)
-fs_naive <- Fscore(Ltrue, Lnaive, 1e-2)
-print(rel_spec)
-print(fs_spec)
-print(rel_naive)
-print(fs_naive)
+relativeError(Ltrue, graph$Lw)
+Fscore(Ltrue, graph$Lw, 1e-1)
+relativeError(Ltrue, Lcgl$Lcgl)
+Fscore(Ltrue, Lcgl$Lcgl, 1e-1)
 
-plot(grid, layout = la_true, vertex.label = NA, vertex.size = 3, main = "True Graph")
-plot(grid_naive, layout = la_naive, vertex.label = NA, vertex.size = 3, main = "Naive Graph")
-plot(grid_est, layout = la_est, vertex.label = NA, vertex.size = 3, main = "spectralGraphTopology Graph")
-corrplot(Wtrue / max(Wtrue), is.corr = FALSE, method = "square", addgrid.col = NA, tl.pos = "n", cl.cex = 1.25)
-corrplot(W / max(W), is.corr = FALSE, method = "square", addgrid.col = NA, tl.pos = "n", cl.cex = 1.25)
-corrplot(Wnaive / max(Wnaive), is.corr = FALSE, method = "square", addgrid.col = NA, tl.pos = "n", cl.cex = 1.25)
+gr = .5 * (1 + sqrt(5))
+setEPS()
+postscript("true_grid.ps", height = 5, width = gr * 3.5)
+plot(grid, layout = la_true, vertex.label = NA, vertex.size = 3)
+dev.off()
+setEPS()
+postscript("sgl_grid.ps", height = 5, width = gr * 3.5)
+plot(grid_spec, layout = la_spec, vertex.label = NA, vertex.size = 3)
+dev.off()
+setEPS()
+postscript("cgl_grid.ps", height = 5, width = gr * 3.5)
+plot(grid_cgl, layout = la_cgl, vertex.label = NA, vertex.size = 3)
+dev.off()
+
--- examples/modular/modular-experiment.R
+++ examples/modular/modular-experiment.R
@@ -1,29 +1,142 @@
 library(igraph)
-library(viridis)
+library(R.matlab)
 library(spectralGraphTopology)
+library(extrafont)
+library(latex2exp)
 
-set.seed(0)
+set.seed(12)
+N_realizations <- 20
+ratios <- c(.5, .75, 1., 5., 10, 30, 100, 250, 500, 1000)
+n_ratios <- c(1:length(ratios))
+rel_err_spec <- array(0, length(ratios))
+rel_err_cgl <- array(0, length(ratios))
+rel_err_cglA <- array(0, length(ratios))
+rel_err_naive <- array(0, length(ratios))
+fscore_spec <- array(0, length(ratios))
+fscore_cgl <- array(0, length(ratios))
+fscore_cglA <- array(0, length(ratios))
+fscore_naive <- array(0, length(ratios))
+
+print("Connecting to MATLAB...")
+matlab <- Matlab(port=9999)
+is_matlab_open <- open(matlab)
+print(is_matlab_open)
+
 N <- 64
-#T <- 30 * N
-T <- 100 * N
-P <- diag(.49, 4) + 0.01
-mgraph <- sample_sbm(N, pref.matrix = P, block.sizes = c(rep(16, 4)))
-E(mgraph)$weight <- runif(gsize(mgraph), min = 1e-1, max = 1)
-Ltrue <- as.matrix(laplacian_matrix(mgraph))
-Y <- MASS::mvrnorm(T, mu = rep(0, N), Sigma = MASS::ginv(Ltrue))
-graph <- learnGraphTopology(cov(Y), K = 1, w0 = "qp", beta = 5)
-W <- graph$W
-W[W < 1e-1] = 0
-est_net <- graph_from_adjacency_matrix(W, mode = "undirected", weighted = TRUE)
-true_net <- graph_from_adjacency_matrix(diag(diag(Ltrue)) - Ltrue, mode = "undirected", weighted = TRUE)
-colors <- inferno(5, begin = 0, end = 1, direction = -1)
-c_scale <- colorRamp(colors)
-E(est_net)$color = apply(c_scale(E(est_net)$weight / max(E(est_net)$weight)), 1, function(x) rgb(x[1]/255, x[2]/255, x[3]/255))
-E(true_net)$color = apply(c_scale(E(true_net)$weight / max(E(true_net)$weight)), 1, function(x) rgb(x[1]/255, x[2]/255, x[3]/255))
-print(relativeError(Ltrue, graph$Lw))
-print(Fscore(Ltrue, graph$Lw, 1e-1))
-print(graph$convergence)
-plot(est_net, vertex.label = NA, vertex.size = 3)
-plot(true_net, vertex.label = NA, vertex.size = 3)
-plot(graph$elapsed_time, graph$obj_fun, type = "b", pch=19, cex=.6, col = scales::alpha("black", .5),
-     xlab = "CPU time [seconds]", ylab = "Objective function")
+K <- 4
+P <- diag(.49, K) + 0.01
+# K-component graph
+A_mask <- matrix(1, N, N) - diag(N)
+setVariable(matlab, A_mask = A_mask)
+
+for (j in n_ratios) {
+  T <- as.integer(ratios[j] * N)
+  cat("\nRunning simulation for", T, "samples per node, T/N = ", ratios[j], "\n")
+  for (n in 1:N_realizations) {
+    mgraph <- sample_sbm(N, pref.matrix = P, block.sizes = c(rep(N / K, K)))
+    A_modular_mask <- as.matrix(laplacian_matrix(mgraph)) != 0
+    setVariable(matlab, A_modular_mask = A_modular_mask)
+    E(mgraph)$weight <- runif(gsize(mgraph), min = 1e-1, max = 3)
+    Ltrue <- as.matrix(laplacian_matrix(mgraph))
+    Y <- MASS::mvrnorm(T, mu = rep(0, N), Sigma = MASS::ginv(Ltrue))
+    S <- cov(Y)
+    s_max <- max(abs(S - diag(diag(S))))
+    alphas <- c(.75 ^ (c(1:14)) * s_max * sqrt(log(N)/ T), 0)
+    setVariable(matlab, S = S)
+    graph <- learnGraphTopology(S, w0 = "naive", K = 1, ub = 32, beta = 4, maxiter = 100000)
+    print(graph$convergence)
+    print(graph$lambda)
+    rel_cgl <- 9999999999
+    rel_cglA <- 9999999999
+    for (alpha in alphas) {
+      setVariable(matlab, alpha = alpha)
+      evaluate(matlab, "[Lcgl,~,~] = estimate_cgl(S, A_mask, alpha, 1e-6, 1e-6, 40, 1)")
+      evaluate(matlab, "[LcglA,~,~] = estimate_cgl(S, A_modular_mask, alpha, 1e-6, 1e-6, 40, 1)")
+      Lcgl <- getVariable(matlab, "Lcgl")
+      LcglA <- getVariable(matlab, "LcglA")
+      if (anyNA(Lcgl$Lcgl) || anyNA(LcglA$LcglA)) {
+        next
+      }
+      tmp_rel_cgl <- relativeError(Ltrue, Lcgl$Lcgl)
+      if (tmp_rel_cgl < rel_cgl) {
+        rel_cgl <- tmp_rel_cgl
+        fs_cgl <- Fscore(Ltrue, Lcgl$Lcgl, 1e-1)
+      }
+      tmp_rel_cglA <- relativeError(Ltrue, LcglA$LcglA)
+      if (tmp_rel_cglA < rel_cglA) {
+        rel_cglA <- tmp_rel_cglA
+        fs_cglA <- Fscore(Ltrue, LcglA$LcglA, 1e-1)
+      }
+    }
+    Lnaive <- MASS::ginv(S)
+    rel_spec = relativeError(Ltrue, graph$Lw)
+    fs_spec = Fscore(Ltrue, graph$Lw, 1e-1)
+    rel_naive = relativeError(Ltrue, Lnaive)
+    fs_naive = Fscore(Ltrue, Lnaive, 1e-1)
+    rel_err_spec[j] <- rel_err_spec[j] + rel_spec
+    fscore_spec[j] <- fscore_spec[j] + fs_spec
+    rel_err_cglA[j] <- rel_err_cglA[j] + rel_cglA
+    fscore_cglA[j] <- fscore_cglA[j] + fs_cglA
+    rel_err_cgl[j] <- rel_err_cgl[j] + rel_cgl
+    fscore_cgl[j] <- fscore_cgl[j] + fs_cgl
+    rel_err_naive[j] <- rel_err_naive[j] + rel_naive
+    fscore_naive[j] <- fscore_naive[j] + fs_naive
+  }
+  rel_err_spec[j] <- rel_err_spec[j] / N_realizations
+  fscore_spec[j] <- fscore_spec[j] / N_realizations
+  rel_err_cglA[j] <- rel_err_cglA[j] / N_realizations
+  fscore_cglA[j] <- fscore_cglA[j] / N_realizations
+  rel_err_cgl[j] <- rel_err_cgl[j] / N_realizations
+  fscore_cgl[j] <- fscore_cgl[j] / N_realizations
+  rel_err_naive[j] <- rel_err_naive[j] / N_realizations
+  fscore_naive[j] <- fscore_naive[j] / N_realizations
+  cat("\n** spectralGraphTopology results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_spec[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_spec[j], "\n")
+  cat("\n** CGL results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_cgl[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_cgl[j], "\n")
+  cat("\n** CGL with A results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_cglA[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_cglA[j], "\n")
+  cat("\n** Naive results **\n")
+  cat("Avg Relative error: ")
+  cat(rel_err_naive[j], "\n")
+  cat("Avg Fscore: ")
+  cat(fscore_naive[j], "\n")
+}
+
+colors <- c("#0B032D", "#843B62", "#F67E7D", "#FFB997")
+gr = .5 * (1 + sqrt(5))
+setEPS()
+postscript("relative_error_modular.ps", family = "ComputerModern", height = 5, width = gr * 3.5)
+plot(n_ratios, rel_err_naive, type = "b", pch=15, cex=.75, ylim=c(0, .5),
+     xlab = TeX("$\\mathit{T} / \\mathit{N}$"), ylab = "Average Relative Error", col = colors[1], xaxt = "n")
+grid()
+lines(n_ratios, rel_err_cgl, type = "b", pch=16, cex=.75, col = colors[2], xaxt = "n")
+lines(n_ratios, rel_err_cglA, type = "b", pch=17, cex=.75, col = colors[3], xaxt = "n")
+lines(n_ratios, rel_err_spec, type = "b", pch=18, cex=.85, col = colors[4], xaxt = "n")
+axis(side = 1, at = n_ratios, labels = ratios)
+legend("topright", legend = c("ISCM", "CGL", TeX("CGL($\\mathbf{A}$)"), "SGL"),
+       col=colors, pch=c(15, 16, 17, 18), lty=c(1, 1, 1, 1), bty="n")
+dev.off()
+embed_fonts("relative_error_modular.ps", outfile="relative_error_modular.ps")
+setEPS()
+postscript("fscore_modular.ps", family = "ComputerModern", height = 5, width = gr * 3.5)
+plot(n_ratios, fscore_naive, ylim=c(.5, 1.), xlab = TeX("$\\mathit{T} / \\mathit{N}$"),
+     ylab = "Average F-score", type = "b", pch=15, cex=.75, col = colors[1], xaxt = "n")
+grid()
+lines(n_ratios, fscore_cgl, type = "b", pch=16, cex=.75, col = colors[2], xaxt = "n")
+lines(n_ratios, fscore_cglA, type = "b", pch=17, cex=.75, col = colors[3], xaxt = "n")
+lines(n_ratios, fscore_spec, type = "b", pch=18, cex=.85, col = colors[4], xaxt = "n")
+axis(side = 1, at = n_ratios, labels = ratios)
+legend("bottomright", legend = c("ISCM", "CGL", TeX("CGL($\\mathbf{A}$)"), "SGL"),
+       col=colors, pch=c(15, 16, 17, 18), lty=c(1, 1, 1, 1), bty="o", bg = "white", box.col = "white")
+dev.off()
+embed_fonts("fscore_modular.ps", outfile="fscore_modular.ps")
