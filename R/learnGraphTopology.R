38dc03d29e7539c1f28d4404a0c5028a33bcf962
mirca <jvmirca@gmail.com>
2019-03-15 10:28:59 -0300

    minor updates on the code for bipartite and joint la estimation

--- R/learnGraphTopology.R
+++ R/learnGraphTopology.R
@@ -26,7 +26,7 @@
 #' @return lambda the optimal value of the eigenvalues
 #' @return U the optimal value of the eigenvectors
 #'
-#' @author Convex group - HKUST
+#' @author Ze Vinicius and Daniel Palomar
 #' @references our paper soon to be submitted
 #' @examples
 #' library(spectralGraphTopology)
@@ -144,35 +144,49 @@ learn_laplacian_matrix <- function(S, is_data_matrix = FALSE, k = 1, w0 = "naive
 
 
 #' @export
-learn_bipartite_graph <- function(S, z = 0, w0 = "naive", alpha = 0., nu = 1e4,
-                                  Lips = NULL, maxiter = 1e4, Awtol = 1e-4, ftol = 1e-4) {
+learn_bipartite_graph <- function(S, is_data_matrix = FALSE, z = 0, w0 = "naive", alpha = 0., nu = 1e4,
+                                  Lips = NULL, m = 7, rho = 1, maxiter = 1e4, abstol = 1e-6, reltol = 1e-4,
+                                  record_objective = FALSE, record_weights = FALSE) {
+  if (is_data_matrix || ncol(S) != nrow(S)) {
+    A <- build_initial_graph(S, m = m)
+    D <- diag(.5 * colSums(A + t(A)))
+    L <- D - .5 * (A + t(A))
+    S <- MASS::ginv(L)
+    is_data_matrix <- TRUE
+  }
   # number of nodes
+  # note now that S is always some sort of similarity matrix
   n <- ncol(S)
   J <- matrix(1/n, n, n)
   # l1-norm penalty factor
   H <- alpha * (2 * diag(n) - matrix(1, n, n))
   K <- S + H
   # compute initial guess
-  w0 <- w_init(w0, MASS::ginv(S))
+  if (is_data_matrix)
+    Sinv <- L
+  else
+    Sinv <- MASS::ginv(S)
+  # if w0 is either "naive" or "qp", compute it, else return w0
+  w0 <- w_init(w0, Sinv)
   if (is.null(Lips))
     Lips <- 1 / min(eigenvalues(L(w0) + J))
   # compute quantities on the initial guess
   Aw0 <- A(w0)
   V0 <- bipartite.V_update(Aw0, z)
   psi0 <- bipartite.psi_update(V0, Aw0)
-  fun_seq <- c()
-  ll_seq <- c()
-  Lips_seq <- c()
-  #w_seq <- list(w0)
+  Lips_seq <- c(Lips)
   time_seq <- c(0)
-  pb = txtProgressBar(min = 0, max = maxiter, initial = 0)
   start_time <- proc.time()[3]
   ll0 <- bipartite.likelihood(Lw = L(w0), K = K, J = J)
   fun0 <- ll0 + bipartite.prior(nu = nu, Aw = Aw0, psi = psi0, V = V0)
-  fun_seq <- c(fun_seq, fun0)
-  ll_seq <- c(ll_seq, ll0)
+  fun_seq <- c(fun0)
+  ll_seq <- c(ll0)
+  if (record_weights)
+    w_seq <- list(Matrix::Matrix(w0, sparse = TRUE))
+  pb <- progress::progress_bar$new(format = "<:bar> :current/:total  eta: :eta  Lipschitz: :Lips ",
+                                   total = maxiter, clear = FALSE, width = 70)
   for (i in 1:maxiter) {
-    setTxtProgressBar(pb, i)
+    pb$tick(token = list(Lipschitz = Lips))
     # we need to make sure that the Lipschitz constant is large enough
     # in order to avoid divergence
     while(1) {
@@ -190,10 +204,10 @@ learn_bipartite_graph <- function(S, z = 0, w0 = "naive", alpha = 0., nu = 1e4,
       Lips_seq <- c(Lips_seq, Lips)
       if (fun0 < fun_t)
         # in case it is in fact larger, then increase Lips and recompute w
-        Lips <- 2 * Lips
+        Lips <- (1 + rho) * Lips
       else {
         # otherwise decrease Lips and get outta here!
-        Lips <- .5 * Lips
+        Lips <- Lips / (1 + rho)
         break
       }
     }
@@ -210,9 +224,13 @@ learn_bipartite_graph <- function(S, z = 0, w0 = "naive", alpha = 0., nu = 1e4,
     fun_seq <- c(fun_seq, fun)
     # compute the relative error and check the tolerance on the Adjacency
     # matrix and on the objective function
-    Awerr <- norm(Aw - Aw0, type="F") / max(1, norm(Aw0, type="F"))
-    ferr <- abs(fun - fun0) / max(1, abs(fun0))
-    if ((Awerr < Awtol && ferr < ftol) && i > 1)
+    if (record_weights)
+      w_seq <- rlist::list.append(w_seq, Matrix::Matrix(w, sparse = TRUE))
+    # check for convergence
+    werr <- abs(w0 - w)
+    has_w_converged <- ((all(werr <= .5 * reltol * (w + w0)) && (reltol > 0)) ||
+                        (all(werr <= abstol) && (abstol > 0)))
+    if (has_w_converged && i > 1)
       break
     # update estimates
     fun0 <- fun
@@ -221,25 +239,28 @@ learn_bipartite_graph <- function(S, z = 0, w0 = "naive", alpha = 0., nu = 1e4,
     psi0 <- psi
     Aw0 <- Aw
   }
-
-  return(list(Laplacian = Lw, Adjacency = Aw, obj_fun = fun_seq, loglike = ll_seq, w = w,
-              psi = psi, V = V, elapsed_time = time_seq, Lips = Lips,
-              Lips_seq = Lips_seq, convergence = !(i == maxiter), nu = nu))
+  results <- list(Laplacian = Lw, Adjacency = Aw, obj_fun = fun_seq, loglike = ll_seq, w = w,
+                  psi = psi, V = V, elapsed_time = time_seq, Lips = Lips,
+                  Lips_seq = Lips_seq, convergence = (i < maxiter), nu = nu)
+  if (record_weights)
+    results$w_seq <- w_seq
+  return(results)
 }
 
 
 #' @export
-learn_adjacency_and_laplacian <- function(S, z = 0, k = 1, w0 = "naive", alpha = 0.,
-                                          beta = 1e4, rho = 1e-2, fix_beta = FALSE, nu = 1e4,
-                                          lb = 0, ub = 1e4, maxiter = 1e4, tol = 1e-4, eig_tol = 1e-9) {
-  if (nrow(S) != ncol(S)) {
+learn_adjacency_and_laplacian <- function(S, is_data_matrix = FALSE, z = 0, k = 1,
+                                          w0 = "naive", m = 7, alpha = 0., beta = 1e4,
+                                          rho = 1e-2, fix_beta = FALSE, nu = 1e4,
+                                          lb = 0, ub = 1e4, maxiter = 1e4, abstol = 1e-6,
+                                          reltol = 1e-4, eig_tol = 1e-9,
+                                          record_weights = FALSE, record_objective = FALSE) {
+  if (is_data_matrix || ncol(S) != nrow(S)) {
     A <- build_initial_graph(S, m = m)
     D <- diag(.5 * colSums(A + t(A)))
     L <- D - .5 * (A + t(A))
     S <- MASS::ginv(L)
     is_data_matrix <- TRUE
-  } else {
-    is_data_matrix <- FALSE
   }
   # number of nodes
   n <- nrow(S)
@@ -259,17 +280,21 @@ learn_adjacency_and_laplacian <- function(S, z = 0, k = 1, w0 = "naive", alpha =
   psi0 <- joint.psi_update(V0, Aw0)
   U0 <- joint.U_update(Lw0, k)
   lambda0 <- joint.lambda_update(lb, ub, beta, U0, Lw0, k)
-  # save objective function value at initial guess
-  ll0 <- joint.likelihood(Lw0, lambda0, K)
-  fun0 <- ll0 + joint.prior(beta, nu, Lw0, Aw0, U0, V0, lambda0, psi0)
-  fun_seq <- c(fun0)
-  ll_seq <- c(ll0)
+  if (record_objective) {
+    # save objective function value at initial guess
+    ll0 <- joint.likelihood(Lw0, lambda0, K)
+    fun0 <- ll0 + joint.prior(beta, nu, Lw0, Aw0, U0, V0, lambda0, psi0)
+    fun_seq <- c(fun0)
+    ll_seq <- c(ll0)
+  }
   beta_seq <- c(beta)
   time_seq <- c(0)
-  pb = txtProgressBar(min = 0, max = maxiter, initial = 0)
   start_time <- proc.time()[3]
+  if (record_weights)
+    w_seq <- list(Matrix::Matrix(w0, sparse = TRUE))
+  pb <- progress::progress_bar$new(format = "<:bar> :current/:total  eta: :eta  beta: :beta  null_eigvals: :null_eigvals",
+                                   total = maxiter, clear = FALSE, width = 100)
   for (i in c(1:maxiter)) {
-    setTxtProgressBar(pb, i)
     w <- joint.w_update(w0, Lw0, Aw0, U0, V0, lambda0, psi0, beta, nu, K)
     Lw <- L(w)
     Aw <- A(w)
@@ -277,21 +302,18 @@ learn_adjacency_and_laplacian <- function(S, z = 0, k = 1, w0 = "naive", alpha =
     V <- joint.V_update(Aw, z)
     lambda <- joint.lambda_update(lb, ub, beta, U, Lw, k)
     psi <- joint.psi_update(V, Aw)
-    # compute negloglikelihood and objective function values
-    ll <- joint.likelihood(Lw, lambda, K)
-    fun <- ll + joint.prior(beta, nu, Lw, Aw, U, V, lambda, psi)
-    # save measurements of time and objective functions
     time_seq <- c(time_seq, proc.time()[3] - start_time)
-    ll_seq <- c(ll_seq, ll)
-    fun_seq <- c(fun_seq, fun)
-    #w_seq <- rlist::list.append(w_seq, w)
-    # compute the relative error and check the tolerance on the Adjacency
-    # matrix and on the objective function
-    # check for convergence
-    #Lwerr <- norm(Lw - Lw0, "F") / norm(Lw0, "F")
-    has_w_converged <- all(abs(w0 - w) <= .5 * tol * (w + w0))
+    if (record_objective) {
+      ll <- joint.likelihood(Lw, lambda, K)
+      fun <- ll + joint.prior(beta, nu, Lw, Aw, U, V, lambda, psi)
+      ll_seq <- c(ll_seq, ll)
+      fun_seq <- c(fun_seq, fun)
+    }
+    if (record_weights)
+      w_seq <- rlist::list.append(w_seq, Matrix::Matrix(w, sparse = TRUE))
     n_zero_eigenvalues <- sum(abs(eigenvalues(Lw)) < eig_tol)
     time_seq <- c(time_seq, proc.time()[3] - start_time)
+    pb$tick(token = list(beta = beta, null_eigvals = n_zero_eigenvalues))
     if (!fix_beta) {
       if (k < n_zero_eigenvalues)
         beta <- (1 + rho) * beta
@@ -299,10 +321,12 @@ learn_adjacency_and_laplacian <- function(S, z = 0, k = 1, w0 = "naive", alpha =
         beta <- beta / (1 + rho)
       beta_seq <- c(beta_seq, beta)
     }
+    werr <- abs(w0 - w)
+    has_w_converged <- ((all(werr <= .5 * reltol * (w + w0)) && (reltol > 0)) ||
+                        (all(werr <= abstol) && (abstol > 0)))
     if (has_w_converged && n_zero_eigenvalues == k)
       break
     # update estimates
-    fun0 <- fun
     w0 <- w
     U0 <- U
     V0 <- V
@@ -311,9 +335,16 @@ learn_adjacency_and_laplacian <- function(S, z = 0, k = 1, w0 = "naive", alpha =
     Lw0 <- Lw
     Aw0 <- Aw
   }
-  return(list(Laplacian = Lw, Adjacency = Aw, obj_fun = fun_seq, loglike = ll_seq, w = w,
-              psi = psi, lambda = lambda, V = V, U = U, elapsed_time = time_seq,
-              beta_seq = beta_seq, convergence = !(i == maxiter)))
+  results <- list(Laplacian = Lw, Adjacency = Aw, w = w, psi = psi,
+                  lambda = lambda, V = V, U = U, elapsed_time = time_seq,
+                  beta_seq = beta_seq, nu = nu, convergence = (i < maxiter))
+  if (record_objective) {
+    results$obj_fun <- fun_seq
+    results$loglike <- ll_seq
+  }
+  if (record_weights)
+    results$w_seq <- w_seq
+  return(results)
 }
 
 
