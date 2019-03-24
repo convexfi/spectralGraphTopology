f7829da214b30030c96db2c95db4a4e19a85e409
mirca <jvmirca@gmail.com>
2019-03-24 05:58:27 -0300

    edges for clr dont make sense

--- benchmarks/cancer-rna/code/cancer-rna-experiment-clr.R
+++ benchmarks/cancer-rna/code/cancer-rna-experiment-clr.R
@@ -30,9 +30,10 @@ for (i in c(1:length(names))) {
   }
 }
 V(net)$cluster <- clusters
-E(net)$color <- apply(as.data.frame(get.edgelist(net)), 1,
-                     function(x) ifelse(V(net)$cluster[x[1]] == V(net)$cluster[x[2]],
-                                        colors[V(net)$cluster[x[1]]], brewer.greys(5)[2]))
+#E(net)$color <- apply(as.data.frame(get.edgelist(net)), 1,
+#                     function(x) ifelse(V(net)$cluster[x[1]] == V(net)$cluster[x[2]],
+#                                        colors[V(net)$cluster[x[1]]], brewer.greys(5)[2]))
+E(net)$color <- "white"
 V(net)$color <- c(colors[1], colors[2], colors[3], colors[4], colors[5])[clusters]
 gr = .5 * (1 + sqrt(5))
 setEPS()
