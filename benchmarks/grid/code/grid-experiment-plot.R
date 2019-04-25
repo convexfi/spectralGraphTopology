c5fe963568da584c0a86ed2938d0cf194e07ad05
mirca <jvmirca@gmail.com>
2019-04-24 21:34:13 -0300

    working on vignette

--- benchmarks/grid/code/grid-experiment-plot.R
+++ benchmarks/grid/code/grid-experiment-plot.R
@@ -77,46 +77,45 @@ legend2 <- c("CGL", "SGL (proposed)")
 xlab <- TeX("$\\mathit{n} / \\mathit{p}$")
 gr = .5 * (1 + sqrt(5))
 setEPS()
-postscript("../latex/figures/relative_error_grid.ps", family = "ComputerModern", height = 6, width = gr * 4.5, pointsize = 14)
+cairo_ps("../latex/figures/relative_error_grid.ps", family = "Times", height = 6, width = gr * 4.5, pointsize = 14)
 plot(n_ratios, rel_err_naive, type = "b", lty = lty[1], pch=pch[1], cex=.75, ylim=c(0, .6),
      xlab = xlab, ylab = "Average Relative Error", col = colors[1], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_naive - sd_rel_err_naive, rev(rel_err_naive + sd_rel_err_naive)),
-#        col = alpha(colors[1], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_naive - sd_rel_err_naive, rev(rel_err_naive + sd_rel_err_naive)),
+        col = alpha(colors[1], alpha = .1), border = NA)
 grid()
 lines(n_ratios, rel_err_qp, type = "b", lty=lty[2], pch=pch[2], cex=.75, col = colors[2], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_qp - sd_rel_err_qp, rev(rel_err_qp + sd_rel_err_qp)),
-#        col = alpha(colors[2], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_qp - sd_rel_err_qp, rev(rel_err_qp + sd_rel_err_qp)),
+        col = alpha(colors[2], alpha = .1), border = NA)
 lines(n_ratios, rel_err_cgl,  type = "b", lty=lty[3], pch=pch[3], cex=.75, col = colors[3], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_cgl - sd_rel_err_cgl, rev(rel_err_cgl + sd_rel_err_cgl)),
-#        col = alpha(colors[3], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_cgl - sd_rel_err_cgl, rev(rel_err_cgl + sd_rel_err_cgl)),
+        col = alpha(colors[3], alpha = .1), border = NA)
 lines(n_ratios, rel_err_sgl, type = "b", lty=lty[4], pch=pch[4], cex=.75, col = colors[4], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_sgl - sd_rel_err_sgl, rev(rel_err_sgl + sd_rel_err_sgl)),
-#        col = alpha(colors[4], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(rel_err_sgl - sd_rel_err_sgl, rev(rel_err_sgl + sd_rel_err_sgl)),
+        col = alpha(colors[4], alpha = .1), border = NA)
 axis(side = 1, at = n_ratios, labels = ratios)
 legend("topright", legend=c(legend1, legend2), col=colors, pch=pch, lty=lty, bty="n")
 dev.off()
-embed_fonts("../latex/figures/relative_error_grid.ps", outfile="../latex/figures/relative_error_grid.ps")
-
+#embed_fonts("../latex/figures/relative_error_grid.ps", outfile="../latex/figures/relative_error_grid.ps")
 setEPS()
-postscript("../latex/figures/fscore_grid.ps", family = "ComputerModern", height = 6, width = gr * 4.5, pointsize = 14)
+cairo_ps("../latex/figures/fscore_grid.ps", family = "Times", height = 6, width = gr * 4.5, pointsize = 14)
 plot(n_ratios, fscore_naive, ylim=c(0, 1.), xlab = xlab, ylab = "Average F-score", type = "b",
      pch=pch[1], lty=lty[1], cex=.75, col = colors[1], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_naive - sd_fscore_naive, rev(fscore_naive + sd_fscore_naive)),
-#        col = alpha(colors[1], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_naive - sd_fscore_naive, rev(fscore_naive + sd_fscore_naive)),
+        col = alpha(colors[1], alpha = .1), border = NA)
 grid()
 lines(n_ratios, fscore_qp,   type = "b", lty=lty[2], pch=pch[2], cex=.75, col = colors[2], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_qp - sd_fscore_qp, rev(fscore_qp + sd_fscore_qp)),
-#        col = alpha(colors[2], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_qp - sd_fscore_qp, rev(fscore_qp + sd_fscore_qp)),
+        col = alpha(colors[2], alpha = .1), border = NA)
 lines(n_ratios, fscore_cgl,  type = "b", lty=lty[3], pch=pch[3], cex=.75, col = colors[3], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_cgl - sd_fscore_cgl, rev(fscore_cgl + sd_fscore_cgl)),
-#        col = alpha(colors[3], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_cgl - sd_fscore_cgl, rev(fscore_cgl + sd_fscore_cgl)),
+        col = alpha(colors[3], alpha = .1), border = NA)
 lines(n_ratios, fscore_sgl, type = "b", lty=lty[4], pch=pch[4], cex=.75, col = colors[4], xaxt = "n")
-#polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_sgl - sd_fscore_sgl, rev(fscore_sgl + sd_fscore_sgl)),
-#        col = alpha(colors[4], alpha = .1), border = NA)
+polygon(x = c(n_ratios, rev(n_ratios)), y = c(fscore_sgl - sd_fscore_sgl, rev(fscore_sgl + sd_fscore_sgl)),
+        col = alpha(colors[4], alpha = .1), border = NA)
 axis(side = 1, at = n_ratios, labels = ratios)
 legend("bottomright", legend=c(legend1, legend2), col=colors, pch=pch, lty=lty, bty="n")
 dev.off()
-embed_fonts("../latex/figures/fscore_grid.ps", outfile="../latex/figures/fscore_grid.ps")
+#embed_fonts("../latex/figures/fscore_grid.ps", outfile="../latex/figures/fscore_grid.ps")
 
 #setEPS()
 #cairo_ps("../latex/figures/recall_grid.ps", family = "Serif", height = 5, width = gr * 3.7, pointsize = 14 )
