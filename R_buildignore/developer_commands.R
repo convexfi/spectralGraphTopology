a743a52b5f9df867e1160f4caef6af8a123630b5
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-04-29 09:47:52 +0800

    Code for tarball generation...

--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -29,3 +29,17 @@ devtools::document()  #to generate all documentation via roxygen
 # Code tests
 devtools::test()
 #covr::package_coverage()  #coverage of tests
+
+
+# CRAN check and submission (http://r-pkgs.had.co.nz/release.html)
+#  checklist: https://kalimu.github.io/post/checklist-for-r-package-submission-to-cran/
+devtools::check()
+rcmdcheck::rcmdcheck()
+devtools::build()
+#devtools::build_win()  #to check under windows
+#R CMD build .  # this is to generate tarball
+#R CMD build . --compact-vignettes=gs+qpdf  
+#R CMD check spectralGraphTopology_0.1.0.tar.gz --as-cran  # this is before submission to CRAN
+#R CMD install spectralGraphTopology_0.1.0.tar.gz
+#submit the tarball directly via the webform: https://cran.r-project.org/submit.html
+
