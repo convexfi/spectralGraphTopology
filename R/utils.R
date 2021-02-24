52e2e53befc2403b8ddc628a7a13978b0d420395
mirca <jvmirca@gmail.com>
2021-02-24 10:31:25 +0800

    [DOC]: clarify docstrings for relative_error and fscore

--- R/utils.R
+++ R/utils.R
@@ -14,24 +14,24 @@ block_diag <- function(...) {
 }
 
 
-#' Computes the relative error between two matrices
+#' Computes the relative error between the true and estimated matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param West estimated matrix
+#' @param Wtrue true matrix
 #' @examples
 #' library(spectralGraphTopology)
 #' X <- L(c(1, 0, 1))
 #' relative_error(X, X)
 #' @export
-relative_error <- function(A, B) {
-  return(norm(A - B, type = "F") / norm(B, type = "F"))
+relative_error <- function(West, Wtrue) {
+  return(norm(West - Wtrue, type = "F") / norm(Wtrue, type = "F"))
 }
 
 
 #' Computes the fscore between two matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param Wtrue true matrix
+#' @param West estimated matrix
 #' @param eps real number such that edges whose values are smaller than eps are
 #'            not considered in the computation of the fscore
 #' @examples
@@ -39,14 +39,14 @@ relative_error <- function(A, B) {
 #' X <- L(c(1, 0, 1))
 #' fscore(X, X)
 #' @export
-fscore <- function(A, B, eps = 1e-4) {
-  return(metrics(A, B, eps)[1])
+fscore <- function(Wtrue, West, eps = 1e-4) {
+  return(metrics(Wtrue, West, eps)[1])
 }
 
 #' Computes the recall between two matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param Wtrue true matrix
+#' @param West estimated matrix
 #' @param eps real number such that edges whose values are smaller than eps are
 #'            not considered in the computation of the fscore
 #' @examples
@@ -54,15 +54,15 @@ fscore <- function(A, B, eps = 1e-4) {
 #' X <- L(c(1, 0, 1))
 #' recall(X, X)
 #' @export
-recall <- function(A, B, eps = 1e-4) {
-  return(metrics(A, B, eps)[2])
+recall <- function(Wtrue, West, eps = 1e-4) {
+  return(metrics(Wtrue, West, eps)[2])
 }
 
 
 #' Computes the specificity between two matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param Wtrue true matrix
+#' @param West estimated matrix
 #' @param eps real number such that edges whose values are smaller than eps are
 #'            not considered in the computation of the fscore
 #' @examples
@@ -70,16 +70,16 @@ recall <- function(A, B, eps = 1e-4) {
 #' X <- L(c(1, 0, 1))
 #' specificity(X, X)
 #' @export
-specificity <- function(A, B, eps = 1e-4) {
-  return(metrics(A, B, eps)[3])
+specificity <- function(Wtrue, West, eps = 1e-4) {
+  return(metrics(Wtrue, West, eps)[3])
 }
 
 
 
 #' Computes the false discovery rate between two matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param Wtrue true matrix
+#' @param West estimated matrix
 #' @param eps real number such that edges whose values are smaller than eps are
 #'            not considered in the computation of the fscore
 #' @examples
@@ -87,15 +87,15 @@ specificity <- function(A, B, eps = 1e-4) {
 #' X <- L(c(1, 0, 1))
 #' fdr(X, X)
 #' @export
-fdr <- function(A, B, eps = 1e-4) {
-  return(metrics(A, B, eps)[6])
+fdr <- function(Wtrue, West, eps = 1e-4) {
+  return(metrics(Wtrue, West, eps)[6])
 }
 
 
 #' Computes the negative predictive value between two matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param Wtrue true matrix
+#' @param West estimated matrix
 #' @param eps real number such that edges whose values are smaller than eps are
 #'            not considered in the computation of the fscore
 #' @examples
@@ -103,15 +103,15 @@ fdr <- function(A, B, eps = 1e-4) {
 #' X <- L(c(1, 0, 1))
 #' npv(X, X)
 #' @export
-npv <- function(A, B, eps = 1e-4) {
-  return(metrics(A, B, eps)[5])
+npv <- function(Wtrue, West, eps = 1e-4) {
+  return(metrics(Wtrue, West, eps)[5])
 }
 
 
 #' Computes the accuracy between two matrices
 #'
-#' @param A first matrix
-#' @param B second matrix
+#' @param Wtrue true matrix
+#' @param West estimated matrix
 #' @param eps real number such that edges whose values are smaller than eps are
 #'            not considered in the computation of the fscore
 #' @examples
@@ -119,8 +119,8 @@ npv <- function(A, B, eps = 1e-4) {
 #' X <- L(c(1, 0, 1))
 #' accuracy(X, X)
 #' @export
-accuracy <- function(A, B, eps = 1e-4) {
-  return(metrics(A, B, eps)[3])
+accuracy <- function(Wtrue, West, eps = 1e-4) {
+  return(metrics(Wtrue, West, eps)[3])
 }
 
 
