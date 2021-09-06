6aeafefcaedf117e31112750b4c622f6b1a641bb
mirca <jvmirca@gmail.com>
2021-09-06 09:51:17 +0800

    update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,6 +7,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // eigval_sym
 arma::vec eigval_sym(arma::mat M);
 RcppExport SEXP _spectralGraphTopology_eigval_sym(SEXP MSEXP) {
